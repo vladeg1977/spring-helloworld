1bd7a689b89a51a73b681146df4a52d312204a53
vladeg1977 <73729840+vladeg1977@users.noreply.github.com>
2021-01-25 13:56:21 +0700

    Cat

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,6 +2,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.concurrent.Callable;
+
 @Configuration
 public class AppConfig {
  
@@ -10,5 +12,11 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        return cat;
     }
 }
